58c2c0014c3327ab6eab7e000b0be439ff4a8a19
vicsotber <victor.soto.berenguer123@gmail.com>
2023-02-04 18:46:34 +0100

    Commit inicial

---
+++ app/src/main/java/com/example/tfg/ui/funcionalidades/FuncionalidadesFragment.kt
@@ -0,0 +1,42 @@
+package com.example.tfg.ui.funcionalidades
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.tfg.databinding.FragmentFuncionalidadesBinding
+
+class FuncionalidadesFragment : Fragment() {
+
+    private var _binding: FragmentFuncionalidadesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentFuncionalidadesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/tfg/ui/funcionalidades/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.tfg.ui.funcionalidades
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "Este es el Fragment de Funcionalidades"
+    }
+    val text: LiveData<String> = _text
+}
